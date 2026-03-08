2ac196309cf686f87732fb60d4ccf900c57f2352
Harsha Vardhan Balusupati <harshavardhanbalusupati761@gmail.com>
2026-03-08 21:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ init_db()
 # =========================
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 # =========================
