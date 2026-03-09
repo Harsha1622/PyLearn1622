760fcba49cfced724186743299341df0f5ec93a0
Harsha Vardhan Balusupati <harshavardhanbalusupati761@gmail.com>
2026-03-09 07:56:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,12 @@ DATABASE = "database.db"
 
 
 def get_db():
-    conn = sqlite3.connect(DATABASE, check_same_thread=False)
+
+    conn = sqlite3.connect(DATABASE)
+
     conn.row_factory = sqlite3.Row
 
-    # Enable foreign keys
+    # enable foreign keys
     conn.execute("PRAGMA foreign_keys = ON")
 
     return conn
